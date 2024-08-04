b5ff79e48635ee2dafb8249e96b899469c19ab54
Tyler Keller <tyler.c.keller@gmail.com>
2024-08-04 15:30:46 -0600

    update

--- Home.py
+++ Home.py
@@ -24,18 +24,36 @@ st.markdown('''
 
 st.dataframe(df)
 
-st.markdown(f'''
-You've watched {df.shape[0]} videos from {df['watch_date'].dt.date.min()} to {df['watch_date'].dt.date.max()}.
-''')
+remove_suspected_livestreams = st.checkbox('Remove suspected livestreams?')
+
+if remove_suspected_livestreams:
+    Q1 = df['length_seconds'].quantile(0.25)
+    Q3 = df['length_seconds'].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    df = df[(df['length_seconds'] >= lower_bound) & (df['length_seconds'] <= upper_bound)]
 
 st.markdown(f'''
-You're most popular channels by total videos are:
+#### You've watched **{df.shape[0]:,} videos** from **{df['watch_date'].dt.date.min()}** to **{df['watch_date'].dt.date.max()}**.
 ''')
-st.dataframe(df['channel_name'].value_counts().reset_index()[:10][['channel_name', 'count']])
 
-st.markdown(f'''
-You're most popular channels by total videos are:
-''')
-st.dataframe(df.groupby(['channel_name'])['length_seconds'].sum().reset_index().sort_values(by='length_seconds', ascending=False))
-# fig = px.pie(df)
-# st.plotly_chart(fig)
+cols = st.columns(2)
+
+with cols[0]:
+    st.markdown(f'''
+    ##### Most popular channels by video count:
+    ''')
+    st.dataframe(df['channel_name'].value_counts().reset_index()[:10][['channel_name', 'count']])
+
+with cols[1]:
+    st.markdown(f'''
+    ##### Most popular channels by video length:
+    ''')
+    st.dataframe(df.groupby(['channel_name'])['length_seconds'].sum().reset_index().sort_values(by='length_seconds', ascending=False).reset_index(drop=True)[:10])
+
+fig = px.box(df, x='length_seconds', title=f'Video Length Boxplot')
+st.plotly_chart(fig)
+
+fig = go.Figure(data=go.Heatmap(z=z, colorscale='Viridis'))
+st.plotly_chart(fig)
